6b46f3a545452d162052b25b78bb10354a4fda02
maziz <mohamed.abdelaziz@edafa.com>
2020-02-20 12:12:55 +0200

    add sql file

---
+++ test.sql
@@ -0,0 +1,4 @@
+ID:1, Title:book one, Author: Author Of Book one, Description: Description Of Book one
+ID:2, Title: Book two, Author: Author Of Book two, Description: Description Of Book two
+ID:3, Title: Book three, Author: Author Of Book three, Description: Description Of Book three
+ID:4, Title: Book four, Author: Author Of Book four, Description: Description Of Book four
